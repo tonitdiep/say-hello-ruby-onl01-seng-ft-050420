8ec34864126b094338abafe80d29798cc7f5bcbc
Toni  Diep <adiep123@gmail.com>
2020-05-04 19:51:04 +0000

    Done.

--- say_hello.rb
+++ say_hello.rb
@@ -1,13 +1,17 @@
-# Build your say_hello method here
-# def say_hello(name, language = "Ruby")
-  # puts "Hello #{name}, you are a #{language} Programmer!"
-# end
 
 def say_hello(name="Ruby Programmer")
   puts "Hello #{name}!"
 end
-
-
-
 #say_hello() -> "Hello Kent Beck"
 #say_hello() -> "Hello Ruby Programmer"
+
+
+
+#def tip_calculator(price, tip=0.2)
+tip_amount = price * tip_amount
+#total = price + tip amount
+#price
+#puts "The tip amount is #{tip amount} and the total is #{total}"
+
+#tip_calculator(60, 0.15)
+#tip_calculator(60)
