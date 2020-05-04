72e1ad6bfb948faf101c1bd932c25ef65f8e3520
Toni  Diep <adiep123@gmail.com>
2020-05-04 19:44:02 +0000

    Done.

--- say_hello.rb
+++ say_hello.rb
@@ -1,1 +1,13 @@
 # Build your say_hello method here
+# def say_hello(name, language = "Ruby")
+  # puts "Hello #{name}, you are a #{language} Programmer!"
+# end
+
+def say_hello(name="Ruby Programmer")
+  puts "Hello #{name}!"
+end
+
+
+
+#say_hello() -> "Hello Kent Beck"
+#say_hello() -> "Hello Ruby Programmer"
